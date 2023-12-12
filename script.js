809c0b3a85e1bf8f3767b3fdc308a812608b651f
Simonwristel <Simon.wristel@elev.ga.ntig.se>
2023-12-12 10:19:02 +0100

    Uppgift 2.1

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello world!")
+const btnHello = document.getElementById("demmo")
+const btn = document.getElementById("button")
+function myFunctionn() {
+    btnHello.innerHTML = "Hej Ändra färg på mig";
+    btn.classList.toggle("newbackground")
+  }
+function myFunction() {
+alert("Hello! I am an alert box!");
+}
